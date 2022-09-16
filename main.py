8cc3094cb9cd021c7e8127656c767115076b6a66
Dari <serhi1334@gmail.com>
2022-09-16 23:32:37 +0300

    added flask html post request which takes information from contacts and sends it to gmail

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-from flask import Flask, render_template
-import requests
+from flask import Flask, render_template,request
+import requests, smtplib
 app = Flask(__name__)
 response = requests.get(url="https://api.npoint.io/2c21b2fca204fb30b798")
 response.raise_for_status()
@@ -10,9 +10,6 @@ def index():
 @app.route('/about')
 def aboutme():
     return render_template('about.html')
-@app.route("/contact")
-def contact():
-    return render_template("contact.html")
 @app.route("/post/<int:post_id>")
 def get_post(post_id):
     requested_post = None
@@ -23,7 +20,29 @@ def get_post(post_id):
             requested_post = posts
     return render_template("post.html", post=requested_post)
 
-
+@app.route('/contact', methods=['GET','POST'])
+def get_data():
+    msg_header=False
+    if request.method == "POST":
+        name = request.form["name"]
+        email = request.form["email"]
+        phone_number = request.form['number']
+        message = request.form['message']
+        print(name)
+        print(email)
+        print(phone_number)
+        print(message)
+        sent_mail(name,email,phone_number,message)
+        msg_header = True
+        return render_template('contact.html', msg_sent=msg_header)
+    return render_template("contact.html", msg_sent=msg_header)
+def sent_mail(name,email,phone,msg):
+    my_email = "serhan.chavdarliev@suborino.eu"
+    password = "gizefsujxnrlxklu"
+    with smtplib.SMTP('smtp.gmail.com') as connection:
+        connection.starttls()
+        connection.login(my_email,password)
+        connection.sendmail(my_email,my_email,msg=f"Subject:New Message\n\n Name: {name} \nEmail: {email} \nPhone-Number: {phone} \nMessage: {msg}")
 
 if __name__ == '__main__':
     app.run(debug=True, host="localhost",port=5000)
